babcd3c17d92a4766b6a363ccd0b5927ca4bff78
luisbebop <luisbebop@gmail.com>
2013-09-30 23:56:27 -0300

    added #ifdef to avoid conflict on includes.

--- net.h
+++ net.h
@@ -1,5 +1,10 @@
+#ifndef _NET_H
+#define _NET_H
+
 void error(const char *msg);
 void uclconnect(int argc, char *argv[]);
 int uclsend(unsigned char * buf, int len);
 int uclreceive(unsigned char * buf, int maxlen);
 void uclclose();
+
+#endif /* _NET_H */
